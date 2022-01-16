03ede6d3b3aa86534894a4f65ff9c37e45eaafbb
flugsvampe <svante@godoy.tech>
2022-01-16 11:21:45 +0100

    nu kan man se vilken ton man spelar och nya ljud

--- musik.py
+++ musik.py
@@ -9,37 +9,46 @@ Keyboard_pin = pin2
 # set up crash sensor
 CrashSensor_pin.set_pull(CrashSensor_pin.PULL_UP)
 
+button = ""
+
 while True:
-    # if crash sensor is pressed, play a note, if not stop the music
-    if CrashSensor_pin.read_digital() == 1:
-        display.set_pixel(0, 0, 0)
-        music.stop()
-
-    else:
-        display.set_pixel(0, 0, 9)
-        music.play("c4:3")
-
     keyboard_value = Keyboard_pin.read_analog()
 
+    old_button = button
+
     if keyboard_value >= 0 and keyboard_value < 30:
-        print("A")
-        music.pitch(440)
-
+        button = "A"
     if keyboard_value >= 30 and keyboard_value < 70:
-        print("B")
-        music.pitch(494)
-
+        button = "B"
     if keyboard_value >= 70 and keyboard_value < 110:
-        print("C")
-        music.pitch(523)
-
+        button = "C"
     if keyboard_value >= 110 and keyboard_value < 150:
-        print("D")
-        music.pitch(587)
-
+        button = "D"
     if keyboard_value >= 150 and keyboard_value < 600:
-        print("E")
-        music.pitch(659)
-
+        button = "E"
     if keyboard_value > 600:
-        music.stop()
+        button = ""
+
+    if button == "":
+        display.clear()
+    else:
+        display.show(button)
+
+    myDuration = 100
+    if CrashSensor_pin.read_digital() != 1:
+        myDuration = 200
+    myWait = True
+
+    if button != old_button:
+        if button == "A":
+            music.pitch(440, duration=myDuration, wait=myWait)
+        if button == "B":
+            music.pitch(494, duration=myDuration, wait=myWait)
+        if button == "C":
+            music.pitch(523, duration=myDuration, wait=myWait)
+        if button == "D":
+            music.pitch(587, duration=myDuration, wait=myWait)
+        if button == "E":
+            music.pitch(659, duration=myDuration, wait=myWait)
+        if button == "":
+            music.stop()
